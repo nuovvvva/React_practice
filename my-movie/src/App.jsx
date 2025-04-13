f4940fe747001a7a4840a20d6907b6bbd70e8570
nuovvvva <kanmengnuo@outlook.com>
2025-04-13 22:05:33 +0800

    Add Routes

--- my-movie/src/App.jsx
+++ my-movie/src/App.jsx
@@ -1,12 +1,21 @@
-import Home from './pages/Home.jsx';
 import './App.css'
+import Favorites from './pages/Favorites';
+import Home from './pages/Home.jsx';
+import {Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar.jsx';
 
 function App() {
   
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
